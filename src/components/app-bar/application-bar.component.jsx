6efbe186533c607db251d9a54dc3b983b380af04
K Mak <kumarajiva08@gmail.com>
2020-11-10 15:02:45 +1100

    Install material-ui. Render AppBar

---
+++ src/components/app-bar/application-bar.component.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import { useStyles } from './application-bar.styles';
+
+const ApplicationBar = () => {
+  const classes = useStyles;
+
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+export default ApplicationBar;
---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ApplicationBar from '../../components/app-bar/application-bar.component';
+
+const HomePage = () => {
+  return (
+    <div>
+      <ApplicationBar />
+    </div>
+  );
+};
+
+export default HomePage;
